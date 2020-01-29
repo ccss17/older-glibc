5fb9a2a968f05078c4c15cf7d77bdbc096515c21
TEST <test@test.t>
2020-01-29 16:32:23 +0000

    forward remaining arg

--- compile_older_glibc.sh
+++ compile_older_glibc.sh
@@ -1,13 +1,21 @@
 if [ -z $1 ] || [ -z $2 ]; then
     echo "Usage: $0 <gblic_version> <file> <out>"
 fi
-echo $@
-VER=$1
-FILE=$2
-OUT=$3
-CC=gcc
-$CC \
-    -L$PWD/$VER/lib \
-    -I$PWD/$VER/include \
-    -Wl,--rpath=$PWD/$VER/lib \
-    -Wl,--dynamic-linker=$PWD/$VER/lib/ld-linux-x86-64.so.2 $FILE -o $OUT
+
+main() {
+    VER=$1
+    FILE=$2
+    OUT=$3
+    CC=gcc
+    shift
+    shift
+    shift
+    $CC \
+        -L$PWD/$VER/lib \
+        -I$PWD/$VER/include \
+        -Wl,--rpath=$PWD/$VER/lib \
+        -Wl,--dynamic-linker=$PWD/$VER/lib/ld-linux-x86-64.so.2 $FILE -o $OUT
+    echo "REMAIN" $@
+}
+
+main $@
